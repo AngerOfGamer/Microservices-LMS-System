9021465d615c97db69e8aac601c6dd2d59b1fae0
vinagunawan <107608324+vinagunawan@users.noreply.github.com>
2025-01-04 14:32:47 +0800

    commit

--- backend/routes/courseRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getCourses } from '../controllers/courseController.js'; 
-
-const router = express.Router();
-
-router.get('/courses', getCourses);
-
-export default router;
