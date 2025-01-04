82aab244688c1a11ab5790371057aac8e6ac524f
Melsy8 <melsyluon@gmail.com>
2025-01-04 17:39:21 +0800

    add content

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import sequelize from './config/database.js';
+
 import absensiRoutes from './routes/absensiRoutes.js';
+import contentRoutes from './routes/contentRoutes.js';
 
 const app = express();
 app.use(bodyParser.json());
 
 // route
-app.use('/absensi', absensiRoutes);
+app.use('/api/absensi', absensiRoutes);
+app.use('/api/content', contentRoutes);
 
+//sync Database
 sequelize.sync()
     .then(() => {
         console.log('Database berhasil disinkronkan');
@@ -17,6 +21,7 @@ sequelize.sync()
         console.error('Terjadi kesalahan saat menyinkronkan database:', err);
     });
 
+// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
