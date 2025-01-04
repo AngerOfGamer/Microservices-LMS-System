c4c4c5faaed99acde98a0da6410a638e98eebb66
vinagunawan <107608324+vinagunawan@users.noreply.github.com>
2025-01-04 14:29:38 +0800

    commit

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user');
-const Class = require('./class');
-const ClassMember = require('./classMember');
-const Content = require('./content');
-const Absensi = require('./absensi');
-const Submission = require('./subission');
-const Nilai = require('./nilai');
-
-module.exports = { User, Class, ClassMember, Content, Absensi, Submission, Nilai };
