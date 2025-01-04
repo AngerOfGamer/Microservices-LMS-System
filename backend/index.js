066e974f391d23556aa1b76e9c8ca7983ff21ce6
jerr-martins <jerry.martins1998@gmail.com>
2025-01-04 21:48:25 +0800

    commit

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,3 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
