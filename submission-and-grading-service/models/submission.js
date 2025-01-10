382ea31e7e230138132bb1ab9ead7a3d8bccf91e
Raphael Immanuel Nugraha <nuelarmungrafael@gmail.com>
2025-01-10 18:10:50 +0800

    Submission and Grading Services

--- submission-and-grading-service/models/submission.js
+++ submission-and-grading-service/models/submission.js
@@ -4,17 +4,17 @@ const getNextSequence = require('../utils/increment');
 const SubmissionSchema = new mongoose.Schema({
   submission_id: { type: Number, unique: true },
   task_title: { type: String, required: true, maxlength: 100 },
-  class_id: { type: Number, ref:'Class', required: true },
-  user_id: { type: Number, ref:'User', required: true },
+  class_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true }, // Relasi ke koleksi Class
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Relasi ke koleksi User
   submission_date: { type: Date, default: Date.now },
   submission_url: { type: String, default: null }
 });
 
 SubmissionSchema.pre('save', async function (next) {
-    if (!this.submission_id) {
-      this.submission_id = await getNextSequence('submission'); 
-    }
-    next();
-  });
+  if (!this.submission_id) {
+    this.submission_id = await getNextSequence('submission');
+  }
+  next();
+});
 
 module.exports = mongoose.model('Submission', SubmissionSchema);
